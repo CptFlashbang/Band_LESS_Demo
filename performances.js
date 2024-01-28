7e03786841ea868d9fd11d7dee7d083df2d9b6e6
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-01-28 20:42:44 +0000

    Add a const for the container element

--- performances.js
+++ performances.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('performances.json')
-    .then(response => response.json())
-    .then(data => createPerformanceElements(data))    
-    .catch(error => console.error('Error fetching JSON:', error));
+        fetch('performances.json')
+        .then(response => response.json())
+        .then(data => createPerformanceElements(data))
+        .catch(error => console.error('Error fetching JSON:', error));
 });
 
 function createPerformanceElements() {
-
+    const performancesContainer = document.getElementById('performances');
 }
