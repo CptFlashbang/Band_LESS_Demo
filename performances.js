743b928a8e87e14fdeaed02fa76a183050fbb71f
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-01-28 20:23:52 +0000

    added catch in case of error

--- performances.js
+++ performances.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch('performances.json')
     .then(response => response.json())
     .then(data => createPerformanceElements(data))    
+    .catch(error => console.error('Error fetching JSON:', error));
 });
 
 function createPerformanceElements() {
